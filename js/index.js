574fdf49095578e2f896891b62aac3eb07fc826b
Stephanie Jane <stephaniejane@shopforage.com>
2026-06-01 15:37:39 -0400

    new corrections primarily to index.js, with help from 1:1 session with Meet

--- js/index.js
+++ js/index.js
@@ -2,17 +2,18 @@ const newItem = document.createElement('footer');
 document.body.appendChild(newItem);
 const today = new Date();
 const thisYear = today.getFullYear();
-const footer = document.querySelector('newItem');
+const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
 copyright.innerHTML = `\u00A9 Stephanie Jane Edwards ${thisYear}`;
 footer.appendChild(copyright);
+//document.body.appendChild(footer);
 
 
-const skills = ["geospatial analysis", "database administration", "Javascript", "painting", "cooking"];
+const skills = ["geospatial analysis", "database administration", "data mining", "grant writing", "project management", "Javascript", "painting", "cooking"];
 const skillsSection = document.querySelector('#Skills');
 const skillsList = skillsSection.querySelector('ul');
 skills.forEach(element => {
     const skill = document.createElement('li');
-    skill.innerHTML = [skill];
-    skillsList.appendChild('element');
+    skill.innerText = element;
+    skillsList.appendChild(skill);
 });
