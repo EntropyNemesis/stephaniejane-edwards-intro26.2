e2bd07a932aa738e1b8298e85e2f7d49210a6051
Stephanie Jane <stephaniejane@shopforage.com>
2026-06-01 12:47:30 -0400

    made some corrections to JS per AI Reviewer feedback

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const newItem = document.createElement('footer');
-newItem.appendChild(footer);
+document.appendChild('newItem');
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('newItem');
@@ -9,10 +9,10 @@ footer.appendChild(copyright);
 
 
 const skills = ["geospatial analysis", "database administration", "Javascript", "painting", "cooking"];
-const skillsSection = document.querySelector('#skills');
-const skillsList = document.querySelector('skillsSection.ul');
+const skillsSection = document.querySelector('#Skills');
+const skillsList = skillsSection.querySelector('ul');
 skills.forEach(element => {
     const skill = document.createElement('li');
-    skill.innerHTML = [skills];
-    skillsList.appendChild('skill');
-});)
+    skill.innerHTML = [skill];
+    skillsList.appendChild('element');
+});
