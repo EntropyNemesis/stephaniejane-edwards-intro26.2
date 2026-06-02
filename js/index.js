d0255dbd52c867d23e4e304f1ac9910c86f51761
Stephanie Jane <stephaniejane@shopforage.com>
2026-06-02 01:00:31 -0400

    changed forEach loop to for loop, added skills style with flexbox, and modified case of skills id to match instructions

--- js/index.js
+++ js/index.js
@@ -6,14 +6,18 @@ const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
 copyright.innerHTML = `\u00A9 Stephanie Jane Edwards ${thisYear}`;
 footer.appendChild(copyright);
-//document.body.appendChild(footer);
 
 
 const skills = ["geospatial analysis", "database administration", "data mining", "grant writing", "project management", "Javascript", "painting", "cooking"];
-const skillsSection = document.querySelector('#Skills');
+const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
-skills.forEach(element => {
+for (let i=0; i < skills.length; i++) {
+    const skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
+/*skills.forEach(element => {
     const skill = document.createElement('li');
     skill.innerText = element;
     skillsList.appendChild(skill);
-});
+}); */
